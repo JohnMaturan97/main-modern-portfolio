a835329156676f53253509befcb34839d8ef9a1b
John Maturan <51711987+JohnMaturan97@users.noreply.github.com>
2021-08-08 12:21:12 -0400

    Update Header.js

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -38,7 +38,7 @@ const Header = () =>  (
         <SocialIcons href="https://www.linkedin.com/in/john-maturan/" target="_blank">
           <AiFillLinkedin size="3rem" />
         </SocialIcons>
-        <SocialIcons href="https://john-maturan-pholio.netlify.app/" target="_blank">
+        <SocialIcons href="https://www.instagram.com/borntocode101/" target="_blank">
           <AiFillInstagram size="3rem"/>
         </SocialIcons>
       </Div3>
