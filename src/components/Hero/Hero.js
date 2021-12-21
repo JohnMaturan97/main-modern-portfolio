a8cc582c29f4c9f99fe0f4bdcf2be1c72c42fedf
John <John_Maturan0206@outlook.com>
2021-12-21 09:35:21 -0500

    PC setup

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,24 +1,35 @@
-import React from 'react';
+import React from "react";
 
-import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponents';
-import Button from '../../styles/GlobalComponents/Button';
-import { LeftSection } from './HeroStyles';
+import {
+  Section,
+  SectionText,
+  SectionTitle,
+} from "../../styles/GlobalComponents";
+import Button from "../../styles/GlobalComponents/Button";
+import { LeftSection } from "./HeroStyles";
 
 const Hero = (props) => (
   <>
     <Section row nopadding>
       <LeftSection>
         <SectionTitle main center>
-        《 ⒷⓄⓇⓃ ⓉⓄ ⒸⓄⒹⒺ 》
+          《 ⒷⓄⓇⓃ ⓉⓄ ⒸⓄⒹⒺ 》
         </SectionTitle>
         <SectionText>
-        Data-driven and a creator who is passionate about building the future of innovation through Computer Science.
+          Data-driven and a creator who is passionate about building the future
+          of innovation through Cloud Native Development.
         </SectionText>
-        
+
         <Button onClick={props.handleClick}>
-        <a id="myLink" href="https://drive.google.com/file/d/1YCt1F8kvgeEo3pQEn7Is2iaAeIaTRtdd/view?usp=sharing" target="_blank" onclick="MyFunction();">Resume</a>
+          <a
+            id="myLink"
+            href="https://drive.google.com/file/d/1YCt1F8kvgeEo3pQEn7Is2iaAeIaTRtdd/view?usp=sharing"
+            target="_blank"
+            onclick="MyFunction();"
+          >
+            Resume
+          </a>
         </Button>
-        
       </LeftSection>
     </Section>
   </>
